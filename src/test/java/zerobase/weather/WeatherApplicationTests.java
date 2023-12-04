15754677ec2b70907dde6b9d75774703cd96af44
pungsuni <dlwldud000223@gmail.com>
2023-12-04 12:43:49 +0900

    first commit

---
+++ src/test/java/zerobase/weather/WeatherApplicationTests.java
@@ -0,0 +1,31 @@
+package zerobase.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class WeatherApplicationTests {
+	@Test
+	void equalTest() {
+	    //given
+		assertEquals(1, 1);
+	    //when
+	    //then
+	}
+	@Test
+	void nullTest() {
+	    //given
+		assertNull(null);
+	    //when
+	    //then
+	}
+	@Test
+	void trueTest() {
+	    //given
+		assertTrue(1==1);
+	    //when
+	    //then
+	}
+}
